f53f06b5cc84637c072fb46b1c0d945272db497a
Sachin Acharya <51048248+sachin-acharya-projects@users.noreply.github.com>
2024-03-27 15:46:27 +0545

    Updated REGEX checking
    New updated 'extract_route_pattern' can match exact pattern like /path/home to /path/home or defined pattern like /path/<address> to /path/countryx
    Direct match -> {}
    Pattern match -> {variable: value}
    No Match -> None

--- Server/Utils/utils/utils.py
+++ Server/Utils/utils/utils.py
@@ -3,13 +3,16 @@ from typing import Any, Dict
 from urllib.parse import unquote
 
 
-__all__ = ['extract_route_pattern']
+__all__ = ["extract_route_pattern"]
+
 
 def extract_route_pattern(pattern: str, string: str) -> Dict[str, str | Any] | None:
     # Removing browser quotes like %20 representing space.
     string = unquote(string)
     # Escape special characters in pattern
     pattern = re.escape(pattern)
+    # Add optional trailing slash to the pattern
+    pattern += r"/*$"
     # Replace <variable_name> with named capturing group regex
     pattern = re.sub(r"<(\w+)>", r"(?P<\1>[^/]+)", pattern)
     # Compile the regex pattern
@@ -25,28 +28,37 @@ def extract_route_pattern(pattern: str, string: str) -> Dict[str, str | Any] | N
 
 if __name__ == "__main__":
     # Test cases
-    pattern1 = "/path/<name>"
-    string1 = "/path/openai"
-    output1 = extract_route_pattern(pattern1, string1)
-    print(output1)  #? Output: {'name': 'openai'}
+    pattern = "/path/<name>"
+    string = "/path/openai"
+    output = extract_route_pattern(pattern, string)
+    print(output)  # ? Output: {'name': 'openai'}
 
-    pattern2 = "/<name>:<age>"
-    string2 = "/openai:12"
-    output2 = extract_route_pattern(pattern2, string2)
-    print(output2)  #? Output: {'name': 'openai', 'age': '12'}
+    pattern = "/<name>:<age>"
+    string = "/openai:12"
+    output = extract_route_pattern(pattern, string)
+    print(output)  # ? Output: {'name': 'openai', 'age': '12'}
 
-    pattern3 = "/path/next/<name>/<age>"
-    string3 = "/path/next/openai/24"
-    output3 = extract_route_pattern(pattern3, string3)
-    print(output3)  #? Output: {'name': 'openai', 'age': '24'}
+    pattern = "/path/next/<name>/<age>"
+    string = "/path/next/openai/24"
+    output = extract_route_pattern(pattern, string)
+    print(output)  # ? Output: {'name': 'openai', 'age': '24'}
 
-    pattern4 = "/path/sachin/s/a/c/h/i/n/<power>/name/<age>"
-    string4 = "/path/sachin/s/a/c/h/i/n/500/name/24"
-    output4 = extract_route_pattern(pattern4, string4)
-    print(output4)  #? Output: {'power': '500', 'age': '24'}
+    pattern = "/path/sachin/s/a/c/h/i/n/<power>/name/<age>"
+    string = "/path/sachin/s/a/c/h/i/n/500/name/24"
+    output = extract_route_pattern(pattern, string)
+    print(output)  # ? Output: {'power': '500', 'age': '24'}
 
+    pattern = "<username>@<domain>.com"
+    string = "sachinacharya@gmail.com"
+    output = extract_route_pattern(pattern, string)
+    print(output)  # ? {'username': 'sachinacharya', 'domain': 'gmail'}
 
-    pattern5 = "<username>@<domain>.com"
-    string5 = "sachinacharya@gmail.com"
-    output5 = extract_route_pattern(pattern5, string5)
-    print(output5)
+    pattern = "/path/name"
+    string = "/path/name"
+    output = extract_route_pattern(pattern, string)
+    print(output)  # ? {}
+
+    pattern = "/"
+    string = "/path/name"
+    output = extract_route_pattern(pattern, string)
+    print(output)  # ? None
